030ae1113f98dc30b77497c0a9eccecffcabaa90
Faisal <phesalbarwez@gmail.com>
2023-11-13 03:11:00 +0530

    signup complete ui done and connected to login page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("com.google.android.gms:play-services-auth:20.7.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
+    implementation("androidx.appcompat:appcompat:1.7.0-alpha03")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
