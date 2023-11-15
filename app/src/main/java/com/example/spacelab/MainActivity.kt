27787140b5b9e111f4af536b6997f2a8b9be43c1
MyusufO <m.yusuf7423@gmail.com>
2023-11-16 03:31:52 +0530

    adding tag feature

--- app/src/main/java/com/example/spacelab/MainActivity.kt
+++ app/src/main/java/com/example/spacelab/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.ui.text.toLowerCase
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.firebase.auth.FirebaseAuth
@@ -22,10 +23,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val add = findViewById<Button>(R.id.addNotes)
-        val currentuser = FirebaseAuth.getInstance().currentUser
-        val logout=findViewById<Button>(R.id.logout)
+        // Initialize UI elements
 
+        val currentuser = FirebaseAuth.getInstance().currentUser
+
+
+        // Initialize RecyclerView and associated components
         notesRecyclerView = findViewById(R.id.notesRecyclerView)
         notesList = mutableListOf()
         notesAdapter = NotesAdapter(notesList)
@@ -34,15 +37,47 @@ class MainActivity : AppCompatActivity() {
         notesRecyclerView.layoutManager = layoutManager
         notesRecyclerView.adapter = notesAdapter
 
+        // Check if the user is logged in
         if (currentuser != null) {
             val mail = currentuser.email
 
+
+
+            // Load existing notes from the database
+            loadNotesFromDatabase(mail)
+        }
+    }
+    override fun onStart() {
+        super.onStart()
+
+        val currentuser = FirebaseAuth.getInstance().currentUser
+
+        if (currentuser == null) {
+            // If the user is not logged in, redirect to the login page
+            val intent = Intent(this, LoginPage::class.java)
+            startActivity(intent)
+            finish()
+        }
+        else{
+
+                val mail = currentuser.email
+
+
+
+                // Load existing notes from the database
+                loadNotesFromDatabase(mail)
+
+            // Logout button functionality
+            val logout=findViewById<Button>(R.id.logout)
             logout.setOnClickListener {
                 FirebaseAuth.getInstance().signOut()
                 val intent = Intent(this, LoginPage::class.java)
                 startActivity(intent)
                 finish()
             }
+
+            // Add notes button functionality
+            val add = findViewById<Button>(R.id.addNotes)
             add.setOnClickListener {
                 val dialog = Dialog(this)
                 dialog.setContentView(R.layout.alertbox)
@@ -51,24 +86,24 @@ class MainActivity : AppCompatActivity() {
                 val okayButton = dialog.findViewById<Button>(R.id.btnOkay)
                 val cancelButton = dialog.findViewById<Button>(R.id.btnCancel)
                 val editText = dialog.findViewById<EditText>(R.id.editText)
-
+                val Tag=dialog.findViewById<EditText>(R.id.Tag)
                 dialog.show()
 
+                // Okay button click listener
                 okayButton.setOnClickListener {
                     val db: FirebaseDatabase = FirebaseDatabase.getInstance()
                     val reference: DatabaseReference = db.getReference("Users")
-
                     reference.get().addOnCompleteListener { task ->
                         if (task.isSuccessful) {
                             val dataSnapshot = task.result
                             if (dataSnapshot != null) {
-                                var childExist = false
-                                var userInput = editText.text.toString()
 
+                                var childExist: Boolean
+                                var userInput = editText.text.toString()
+                                var tagtext= Tag.text.toString().lowercase()
                                 for (snapshot in dataSnapshot.children) {
                                     val userKey = snapshot.key
                                     val userEmail = snapshot.child("email").getValue()
-
                                     if (userEmail == mail) {
                                         val reference1 = db.getReference("Users/$userKey")
                                         val child = reference1.child("notes").child(userInput)
@@ -83,11 +118,13 @@ class MainActivity : AppCompatActivity() {
                                                         "Title Already Taken",
                                                         Toast.LENGTH_SHORT
                                                     ).show()
-                                                } else {
+                                                }
+                                                else {
                                                     // Child node doesn't exist
 
                                                     val intent = Intent(this@MainActivity, CreateNote::class.java)
-                                                    intent.putExtra("path","Users/$userKey/notes/$userInput")
+                                                    child.child("tag").setValue(tagtext)
+                                                    intent.putExtra("path","Users/$userKey/notes/$userInput/text")
                                                     startActivity(intent)
                                                 }
                                             }
@@ -99,7 +136,8 @@ class MainActivity : AppCompatActivity() {
                                     }
                                 }
                             }
-                        } else {
+                        }
+                        else {
                             // Handle the error
                             println("Error: ${task.exception?.message}")
                         }
@@ -107,15 +145,15 @@ class MainActivity : AppCompatActivity() {
                     dialog.dismiss()
                 }
 
+                // Cancel button click listener
                 cancelButton.setOnClickListener {
                     dialog.dismiss()
                 }
             }
-
-            loadNotesFromDatabase(mail)
         }
     }
 
+    // Function to display notes on Screen
     private fun loadNotesFromDatabase(userEmail: String?) {
         val db: FirebaseDatabase = FirebaseDatabase.getInstance()
         val reference: DatabaseReference = db.getReference("Users")
@@ -130,7 +168,7 @@ class MainActivity : AppCompatActivity() {
                     if (userEmail == userEmailFromDB) {
                         for (noteSnapshot in snapshot.child("notes").children) {
                             val noteTitle = noteSnapshot.key
-                            val noteContent = noteSnapshot.getValue(String::class.java)
+                            val noteContent = noteSnapshot.child("text").getValue(String::class.java)
 
                             if (noteTitle != null && noteContent != null) {
                                 notesList.add(Note(noteTitle, noteContent))
