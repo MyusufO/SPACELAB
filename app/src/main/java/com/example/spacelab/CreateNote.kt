ee2e9bae6cde24d0e16392ab6e5605f00b7148f9
MyusufO <m.yusuf7423@gmail.com>
2023-11-19 02:51:17 +0530

    Image removal feature added

--- app/src/main/java/com/example/spacelab/CreateNote.kt
+++ app/src/main/java/com/example/spacelab/CreateNote.kt
@@ -13,7 +13,6 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.storage.FirebaseStorage
@@ -21,10 +20,8 @@ import com.google.firebase.storage.StorageReference
 
 class CreateNote : AppCompatActivity() {
 
-    // Constant for identifying image pick request
     private val PICK_IMAGE_REQUEST = 1
 
-    // UI elements
     private lateinit var mButtonUpload: Button
     private lateinit var mImageView: RecyclerView
     private lateinit var editText: EditText
@@ -32,27 +29,21 @@ class CreateNote : AppCompatActivity() {
     private lateinit var cancelButton: Button
     private lateinit var removeImageButton: Button
 
-    // Firebase storage and database references
     private lateinit var mStorageRef: StorageReference
     private lateinit var mDatabaseRef: DatabaseReference
-
-    // List to store selected image URIs
     private val imageList = mutableListOf<Uri?>()
-
-    // Variable to store the currently selected image URI
     private var mImageUri: Uri? = null
+
+    private lateinit var recyclerItemClickListener: RecyclerItemClickListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Retrieve username from intent extras
         val extras = intent.extras
         val username = extras?.getString("path")
 
-        // Set the layout for this activity
         setContentView(R.layout.activity_create_note)
 
-        // Initialize UI elements
         mButtonUpload = findViewById(R.id.image)
         mImageView = findViewById(R.id.recyclerView)
         editText = findViewById(R.id.editText)
@@ -60,11 +51,9 @@ class CreateNote : AppCompatActivity() {
         cancelButton = findViewById(R.id.cancelButton)
         removeImageButton = findViewById(R.id.removeImageButton)
 
-        // Initialize Firebase storage and database references
         mStorageRef = FirebaseStorage.getInstance().getReference("uploads")
         mDatabaseRef = FirebaseDatabase.getInstance().getReference(username!!)
 
-        // Initialize RecyclerView
         mImageView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
         mImageView.addItemDecoration(
             DividerItemDecoration(
@@ -73,56 +62,50 @@ class CreateNote : AppCompatActivity() {
             )
         )
 
-        // Set click listener for the "Remove Image" button
         removeImageButton.setOnClickListener {
-            // Show a message to inform the user to click on an image to remove
             Toast.makeText(this, "Click on an image to remove", Toast.LENGTH_SHORT).show()
-        }
 
-        // Set a click listener on the RecyclerView items to handle image removal
-        mImageView.addOnItemTouchListener(
-            RecyclerItemClickListener(this, mImageView,
+            recyclerItemClickListener = RecyclerItemClickListener(this, mImageView,
                 object : RecyclerItemClickListener.OnItemClickListener {
                     override fun onItemClick(view: View, position: Int) {
-                        // Call the removeImage function with the selected position
                         removeImage(position)
                     }
                 })
-        )
-
-        // Set click listener for the "Upload Image" button
-        mButtonUpload.setOnClickListener {
-            openFileChooser()
+            mImageView.addOnItemTouchListener(recyclerItemClickListener)
         }
 
-        // Set click listener for the "Save" button
+        mButtonUpload.setOnClickListener {
+            openFileChooser()
+
+            if (::recyclerItemClickListener.isInitialized) {
+                mImageView.removeOnItemTouchListener(recyclerItemClickListener)
+            }
+        }
+
+        val imageAdapter = ImageAdapter(this, imageList)
+        mImageView.adapter = imageAdapter
+
         saveButton.setOnClickListener {
             val inputText = editText.text.toString()
             val userKey = extractUserKey(username)
             val imagesPath = "Images"
 
-            // Save text in Firebase Database
             val reference: DatabaseReference =
                 FirebaseDatabase.getInstance().getReference(username)
             reference.child("text").setValue(inputText)
 
-            // Save selected images in Firebase Storage and their download URLs in Firebase Database
             for (i in imageList.indices) {
                 val imagePath = "$imagesPath/$i"
                 val imagesReference = mStorageRef.child("$imagePath/$userKey")
 
-                // Upload image to Firebase Storage
                 imagesReference.putFile(imageList[i]!!)
                     .addOnSuccessListener { taskSnapshot ->
-                        // Get the download URL for the uploaded image
                         taskSnapshot.storage.downloadUrl.addOnSuccessListener { uri ->
-                            // Save the download URL in Firebase Database
                             val imagesReference = mDatabaseRef.child(imagePath)
                             imagesReference.setValue(uri.toString())
                         }
                     }
                     .addOnFailureListener { e ->
-                        // Handle error uploading image
                         Toast.makeText(
                             this,
                             "Error uploading image: ${e.message}",
@@ -131,27 +114,18 @@ class CreateNote : AppCompatActivity() {
                     }
             }
 
-            // Display a toast message indicating that the note is saved
             Toast.makeText(this, "Note saved", Toast.LENGTH_SHORT).show()
 
-            // Redirect to the main activity
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
 
-        // Set click listener for the "Cancel" button
         cancelButton.setOnClickListener {
-            // Redirect to the main activity without saving
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-
-        // Set up RecyclerView adapter
-        val imageAdapter = ImageAdapter(this, imageList)
-        mImageView.adapter = imageAdapter
     }
 
-    // Function to open the file chooser for selecting images
     private fun openFileChooser() {
         val intent = Intent()
         intent.type = "image/*"
@@ -159,27 +133,21 @@ class CreateNote : AppCompatActivity() {
         startActivityForResult(intent, PICK_IMAGE_REQUEST)
     }
 
-    // Override function to handle the result from the file chooser
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.data != null) {
-            // Retrieve the selected image URI
             mImageUri = data.data
-
-            // Update the RecyclerView with the selected image
             imageList.add(mImageUri)
             mImageView.adapter?.notifyDataSetChanged()
         }
     }
 
-    // Function to extract the user key from the provided string
     private fun extractUserKey(inputString: String): String? {
         val regex = Regex("""Users/([^/]+)/notes/\w+""")
         val matchResult = regex.find(inputString)
         return matchResult?.groups?.get(1)?.value
     }
 
-    // Function to remove a selected image from the list
     private fun removeImage(position: Int) {
         if (position in 0 until imageList.size) {
             imageList.removeAt(position)
--- app/src/main/java/com/example/spacelab/RecyclerItemClickListener.kt
+++ app/src/main/java/com/example/spacelab/RecyclerItemClickListener.kt
@@ -8,16 +8,19 @@ import android.view.MotionEvent
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
+// Custom RecyclerView item click listener that implements RecyclerView.OnItemTouchListener
 class RecyclerItemClickListener(
     context: Context,
     recyclerView: RecyclerView,
     private val mListener: OnItemClickListener?
 ) : RecyclerView.OnItemTouchListener {
 
+    // Interface for defining item click callback
     interface OnItemClickListener {
         fun onItemClick(view: View, position: Int)
     }
 
+    // GestureDetector to detect gestures, in this case, single taps
     private val mGestureDetector: GestureDetector =
         GestureDetector(context, object : GestureDetector.SimpleOnGestureListener() {
             override fun onSingleTapUp(e: MotionEvent): Boolean {
@@ -25,19 +28,29 @@ class RecyclerItemClickListener(
             }
         })
 
+    // Initialization block: add this listener to the provided RecyclerView
     init {
         recyclerView.addOnItemTouchListener(this)
     }
 
+    // Called when a touch event is intercepted in the RecyclerView
     override fun onInterceptTouchEvent(rv: RecyclerView, e: MotionEvent): Boolean {
+        // Find the child view at the touched coordinates
         val child = rv.findChildViewUnder(e.x, e.y)
+
+        // Check if there's a listener, the gesture detector detects a single tap, and a child view is present
         if (child != null && mListener != null && mGestureDetector.onTouchEvent(e)) {
+            // Notify the listener of the item click, providing the clicked view and its position
             mListener.onItemClick(child, rv.getChildAdapterPosition(child))
         }
+
+        // Always return false to allow further handling of touch events by the RecyclerView
         return false
     }
 
+    // Unused method, as we only need to intercept touch events
     override fun onTouchEvent(rv: RecyclerView, e: MotionEvent) {}
 
+    // Unused method, as we don't need to handle disallowing the interception of touch events
     override fun onRequestDisallowInterceptTouchEvent(disallowIntercept: Boolean) {}
 }
