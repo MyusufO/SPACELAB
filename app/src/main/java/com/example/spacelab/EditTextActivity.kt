c619f91da35fbcb80555a2857d406666ad216f14
MyusufO <m.yusuf7423@gmail.com>
2023-11-19 14:07:17 +0530

    Images can be removed in EditText as well

--- app/src/main/java/com/example/spacelab/EditTextActivity.kt
+++ app/src/main/java/com/example/spacelab/EditTextActivity.kt
@@ -1,9 +1,12 @@
-package com.example.spacelab
+package  com.example.spacelab
 
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,6 +19,15 @@ class EditTextActivity : AppCompatActivity() {
     private lateinit var imageAdapter: ImageAdapter
     private val imageList = mutableListOf<Uri?>()
 
+    // Activity result contract for image selection
+    private val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+        // Handle the result of image selection
+        uri?.let {
+            imageList.add(it)
+            imageAdapter.notifyDataSetChanged()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_text)
@@ -24,6 +36,8 @@ class EditTextActivity : AppCompatActivity() {
         recyclerView = findViewById(R.id.recyclerView)
         val saveButton: Button = findViewById(R.id.saveButton)
         val cancelButton: Button = findViewById(R.id.cancelButton)
+        val addImageButton: Button = findViewById(R.id.addImageButton)
+        val removeImageButton: Button = findViewById(R.id.removeImageButton)
 
         val pathAsString = intent.getStringExtra("path")
 
@@ -54,6 +68,29 @@ class EditTextActivity : AppCompatActivity() {
             println("Error fetching images: ${it.message}")
         }
 
+        // Add Image button click listener
+        addImageButton.setOnClickListener {
+            // Open the device's gallery to select an image
+            getContent.launch("image/*")
+        }
+
+        // Remove Image button click listener
+        removeImageButton.setOnClickListener {
+            // Display a message to guide the user on how to remove an image
+            showToastForRemoveImage()
+
+            // Initialize the recycler item click listener for removal
+            val recyclerItemClickListener = RecyclerItemClickListener(this, recyclerView,
+                object : RecyclerItemClickListener.OnItemClickListener {
+                    override fun onItemClick(view: android.view.View, position: Int) {
+                        removeImage(position)
+                    }
+                })
+
+            // Add the item click listener
+            recyclerView.addOnItemTouchListener(recyclerItemClickListener)
+        }
+
         // Save button click listener
         saveButton.setOnClickListener {
             // Save the edited text to the database using the path
@@ -61,6 +98,16 @@ class EditTextActivity : AppCompatActivity() {
             reference.child("text").setValue(newText).addOnSuccessListener {
                 // Handle the success (optional)
                 println("Text updated successfully.")
+            }.addOnFailureListener {
+                // Handle the error
+                println("Error: ${it.message}")
+            }
+
+            // Save images to the database using the path
+            val imagesRef = reference.child("Images")
+            imagesRef.setValue(imageList.map { it.toString() }).addOnSuccessListener {
+                // Handle the success (optional)
+                println("Images updated successfully.")
             }.addOnFailureListener {
                 // Handle the error
                 println("Error: ${it.message}")
@@ -74,6 +121,21 @@ class EditTextActivity : AppCompatActivity() {
         cancelButton.setOnClickListener {
             // Finish the activity without saving
             finish()
+        }
+    }
+
+    private fun showToastForRemoveImage() {
+        // Display a message to guide the user on how to remove an image
+        Toast.makeText(this, "Click on an image to remove", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun removeImage(position: Int) {
+        // Remove the image at the specified position
+        if (position in 0 until imageList.size) {
+            imageList.removeAt(position)
+            imageAdapter.notifyDataSetChanged()
+        } else {
+            Toast.makeText(this, "Invalid image position", Toast.LENGTH_SHORT).show()
         }
     }
 }
