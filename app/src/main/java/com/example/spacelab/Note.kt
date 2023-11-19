a626e98c1417c13ca0ab3a1cced9fbdb8ef43ad5
MyusufO <m.yusuf7423@gmail.com>
2023-11-19 21:02:43 +0530

    EditText

--- app/src/main/java/com/example/spacelab/Note.kt
+++ app/src/main/java/com/example/spacelab/Note.kt
@@ -1,8 +1,7 @@
 package com.example.spacelab
 
-// Note.kt
-
 data class Note(
     val title: String = "",
-    val content: String = ""
+    val content: String = "",
+    val color: String = "" // Add this line for the color property
 )
